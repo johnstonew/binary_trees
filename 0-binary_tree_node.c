f208d56502e48fe15088cc1b5721195ef0f36b48
Johnstone Wahome <johnstonewahome@gmail.com>
2023-06-01 14:21:18 +0300

    0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - binary tree node.
+ * @parent: pointer to the parent of the node tho be created.
+ * @value: value of the new node.
+ *
+ * Return: If error occurs return NULL else pointer of new node
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
