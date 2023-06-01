fc20e9deae6dd74afd0d8ed74fa827300116abdb
Johnstone Wahome <johnstonewahome@gmail.com>
2023-06-01 19:00:38 +0300

    added parent

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,15 +10,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-
-	binary_tree_t *parent = node->parent;
-
-	if (parent->left == node)
-		return (parent->right);
-
-	if (parent->right == node)
-		return (parent->left);
-
-	return (NULL);
+	
+	if (node->parent->left == node)
+		return (node->parent->right);
+	
+	return (node->parent->left);
 }
 
