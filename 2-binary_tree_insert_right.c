ea864f4df1d8623f13e103aa87e829b0d738e9de
Johnstone Wahome <johnstonewahome@gmail.com>
2023-06-01 15:01:46 +0300

    updates

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,8 +20,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent->right != NULL)
 	{
-		parent->right->parent = new_node;
 		new_node->right = parent->right;
+		parent->right->parent = new_node;
 	}
 
 	parent->right = new_node;
