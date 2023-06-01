63e2df69bcb1e9d7126188bd9b724e1188b097e6
Johnstone Wahome <johnstonewahome@gmail.com>
2023-06-01 18:39:52 +0300

    height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,26 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the tree
+ *
+ * Return: If error occurs return NULL else pointer of new node
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
 	if (tree == NULL)
 		return (0);
 
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
-	int balance_factor = left_height - right_height;
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
-	return (balance_factor);
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
-
